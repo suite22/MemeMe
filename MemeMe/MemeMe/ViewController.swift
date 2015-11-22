88333234055e8affafd99c374644244aafe7aeca
Ben Goertz <goertz@pndlm.com>
2015-11-22 10:38:50 -0800

    Initial Xcode project creation.

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Ben Goertz on 11/22/15.
+//  Copyright © 2015 suite22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
