c6317ab5d016ff12ba21572f23f63900e73771ae
Ben Goertz <goertz@pndlm.com>
2015-11-22 21:05:13 -0800

    Starting the data model.

---
+++ MemeMe/MemeMe/DataModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataModel.swift
+//  MemeMe
+//
+//  Created by Ben Goertz on 11/22/15.
+//  Copyright © 2015 suite22. All rights reserved.
+//
+
+import Foundation
+
+public class DataModel {
+	
+}
